2e78bec761469bbb71b92bf9b4d5ed82ca5d0e7b
이승주 <105261571+leesj8116@users.noreply.github.com>
2023-10-06 14:46:02 +0900

    Time: 2 ms (65.92%), Space: 44 MB (98.08%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -2,15 +2,22 @@ class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int length = nums1.length + nums2.length;
 		int[] arr = new int[length];
-		for (int i = 0 ; i < nums1.length; i++) {
-			arr[i] = nums1[i];
-		}
-		for (int i = 0; i < nums2.length; i++) {
-			arr[nums1.length + i] = nums2[i];
-		}
 
-		// sorted
-		arr = Arrays.stream(arr).sorted().toArray();
+        // 개선 1. 두 배열을 합칠 때 부터 정렬을 적용해서 개선
+        int p1 = 0; int p2 = 0;
+        int index = 0;
+        while((p1 < nums1.length) && (p2 < nums2.length)) {
+            // 비교 후 더 작은 수 부터 삽입
+            arr[index++] = (nums1[p1] < nums2[p2]) ? nums1[p1++] : nums2[p2++];
+        }
+
+        // 한쪽 배열의 길이가 짧아 다 삽입하지 못한 경우, 남은 배열을 마저 삽입
+        while (p1 < nums1.length) {
+            arr[index++] = nums1[p1++];
+        }
+        while (p2 < nums2.length) {
+            arr[index++] = nums2[p2++];
+        }
 
 		if (length % 2 == 0) {
 			return ((arr[length / 2] + arr[(length / 2) - 1]) / 2.0f);
